777ee79a7d2b4d13d00f20d537ef2dbbbbdf6cd4
Justin <jwarnes@gmail.com>
2012-08-28 14:11:51 -0400

    Adds validater class

---
+++ DPS Calc/DPS Calc/validater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DPS_Calc
+{
+    class validater
+    {
+
+        public static bool isInt(TextBox textBox)
+        {
+            try
+            {
+                Convert.ToInt16(textBox.Text);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
